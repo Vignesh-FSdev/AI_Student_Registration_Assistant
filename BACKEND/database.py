876ae56b38f368d285128f4a418d363d68a5bf30
Vignesh <vigneshaifsdev@gmail.com>
2026-07-11 13:49:42 +0530

    Project cleanup and added professional files

--- BACKEND/database.py
+++ BACKEND/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb+srv://vignesh:<db_password>@cluster0.rn5lrgh.mongodb.net/?appName=Cluster0")
 
 db = client["student_registration"]
 
